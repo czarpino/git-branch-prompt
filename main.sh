987c3d1fac0021eed806ea675f7ac6a32997bc37
Czar <inbox@czarpino.com>
2017-12-09 18:59:28 +0800

    Indent using spacces

--- main.sh
+++ main.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 function get_git_branch() {
-	if [ "$PWD" != "$MYOLDPWD" ] || [ $is_checkout ]; then
-		MYOLDPWD="$PWD"
-		git_branch="$(git rev-parse --abbrev-ref HEAD 2> /dev/null)"
-		unset is_checkout
-	fi
+    if [ "$PWD" != "$MYOLDPWD" ] || [ $is_checkout ]; then
+        MYOLDPWD="$PWD"
+        git_branch="$(git rev-parse --abbrev-ref HEAD 2> /dev/null)"
+        unset is_checkout
+    fi
 }
 
 function capture_command() {
-	if [[ "$BASH_COMMAND" == "git checkout"* ]]; then
-		is_checkout=true
-	fi
+    if [[ "$BASH_COMMAND" == "git checkout"* ]]; then
+        is_checkout=true
+    fi
 }
 
 trap capture_command DEBUG
